b0946ef6c5b62f68cdc238dba8e90bc59317f99f
Leekibeom <lkb951103@gmail.com>
2021-11-21 12:34:14 +0900

    metadata-refac

--- src/components/ResultSection/index.js
+++ src/components/ResultSection/index.js
@@ -1,26 +1,34 @@
 import React from "react";
 import styled from "styled-components";
-import { SocialButtonGroup } from "components";
+import RESULT from "./result";
 
-const ResultTitle = styled.h1`
-  font-size: 54px;
+const SectionTitle = styled.h1`
+  font-size: 64px;
   font-weight: bold;
-  margin: 8px 0px;
+  margin-top: 0px;
+  margin-bottom: 56px;
   text-align: center;
 `;
 
-const Score = styled.p`
-  font-size: 120px;
-  margin: 40px;
-  text-align: center;
-  color: ${(props) => props.theme.primaryColor100};
+const StyledImage = styled.img`
+  border-radius: 16px;
+  opacity: 80%;
+  margin-bottom: 16px;
+  max-width: 100%;
+  display: block;
 `;
 
-const ResultSection = ({ conScore }) => (
+const ResultTitle = styled.div`
+  font-size: 24px;
+  margin-bottom: 56px;
+  text-align: center;
+`;
+
+const ResultSection = ({ cCode }) => (
   <>
-    <ResultTitle>당신의 점수는?</ResultTitle>
-    <Score>{conScore} 점</Score>
-    <SocialButtonGroup></SocialButtonGroup>
+    <SectionTitle>당신은 혹시?</SectionTitle>
+    <StyledImage src={RESULT[cCode].src} />
+    <ResultTitle>{RESULT[cCode].title}</ResultTitle>
   </>
 );
 export default ResultSection;
